fbd223426aee2e3a3ad4e6f3db7f666da406387c
Maaloum <50062179+maaloum@users.noreply.github.com>
2022-08-17 21:24:31 +0300

    fix linters and webpack

--- webpack.common.js
+++ webpack.common.js
@@ -3,37 +3,29 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'index_bundle.js',
-  },
+
   module: {
-
     rules: [
-
       {
-
         test: /\.css$/i,
-
-        use: ['style-loader', 'css-loader'],
-
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
       },
-
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
-
   },
   plugins: [
-
     new HtmlWebpackPlugin({
-
       template: './src/index.html',
-
     }),
-
   ],
-  devServer: {
-
-    static: './dist',
-
-  },
 };
